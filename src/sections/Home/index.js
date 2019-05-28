fc7ab673c92ecf4d5e002f5ab67df985da67ae3a
Josh <ejosh.manuel@gmail.com>
2019-05-27 19:04:59 -0700

    home content added

--- src/sections/Home/index.js
+++ src/sections/Home/index.js
@@ -1,11 +1,14 @@
 import React, { Component } from "react";
+import "./style.css";
 
 class Home extends Component {
 
   render() {
     return (
       <React.Fragment>
-
+        <div className="title-1">Hi,</div>
+        <div className="title-2">I'm a Web Developer</div>
+        <div className="subtitle">FULL-STACK DEVELOPER / MERN STACK / WEB DESIGNER</div>
       </React.Fragment>
     );
   }
